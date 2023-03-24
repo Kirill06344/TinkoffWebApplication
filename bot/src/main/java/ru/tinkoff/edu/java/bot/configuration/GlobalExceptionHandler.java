2853c3e94bdf5475f50245bb5062993f5f0c8279
dasu <kirill12093@mail.ru>
2023-03-25 01:38:08 +0300

    add exceptionsHandler

---
+++ bot/src/main/java/ru/tinkoff/edu/java/bot/configuration/GlobalExceptionHandler.java
@@ -0,0 +1,39 @@
+package ru.tinkoff.edu.java.bot.configuration;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import ru.tinkoff.edu.java.bot.web.controllers.BotController;
+import ru.tinkoff.edu.java.bot.web.dto.ApiErrorResponse;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler{
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiErrorResponse> handleException(Exception e) {
+        return ResponseEntity.status(400)
+            .body(
+                ApiErrorResponse.builder()
+                    .description("Некорректные параметры запроса")
+                    .exceptionName(e.getClass().getName())
+                    .exceptionMessage(e.getMessage())
+                    .stacktrace(
+                        Arrays.stream(e.getStackTrace())
+                            .map(StackTraceElement::getClassName)
+                            .collect(Collectors.toList())
+                    )
+                    .code("400")
+                    .build()
+            );
+    }
+}
---
+++ scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/configuration/GlobalExceptionHandler.java
@@ -0,0 +1,40 @@
+package ru.tinkoff.edu.java.scrapper.configuration;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import ru.tinkoff.edu.java.scrapper.web.dto.ApiErrorResponse;
+
+@RestControllerAdvice (
+    basePackages = "ru.tinkoff.edu.java.scrapper.web.controllers"
+)
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler({MethodArgumentTypeMismatchException.class, MethodArgumentNotValidException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ApiErrorResponse> handleException(Exception e) {
+        return ResponseEntity.status(400)
+            .body(
+                ApiErrorResponse.builder()
+                    .description("Некорректные параметры запроса")
+                    .exceptionName(e.getClass().getName())
+                    .exceptionMessage(e.getMessage())
+                    .stacktrace(
+                        Arrays.stream(e.getStackTrace())
+                            .map(StackTraceElement::getClassName)
+                            .collect(Collectors.toList())
+                    )
+                    .code("400")
+                    .build()
+            );
+    }
+
+}
