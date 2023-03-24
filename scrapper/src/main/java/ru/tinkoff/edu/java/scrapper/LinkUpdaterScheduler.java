5a4542ec302a2a9b4265900d0bb7f8e3964dca3a
dasu <kirill12093@mail.ru>
2023-03-25 01:39:06 +0300

    add scheduled and swagger properties

---
+++ scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/LinkUpdaterScheduler.java
@@ -0,0 +1,17 @@
+package ru.tinkoff.edu.java.scrapper;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class LinkUpdaterScheduler {
+
+    @Scheduled(fixedRateString = "${app.scheduler.interval}")
+    public void update() {
+      log.info("Some update...");
+    }
+
+}
