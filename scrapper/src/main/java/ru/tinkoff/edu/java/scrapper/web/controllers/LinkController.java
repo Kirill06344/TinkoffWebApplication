70cfb335cb2b21b76a95c0e630e8b7b50c60e6b5
dasu <kirill12093@mail.ru>
2023-03-27 20:14:53 +0300

    fix(webclient) : cg

--- scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/web/controllers/LinkController.java
+++ scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/web/controllers/LinkController.java
@@ -1,10 +1,8 @@
 package ru.tinkoff.edu.java.scrapper.web.controllers;
 
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -16,15 +14,11 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
-import ru.tinkoff.edu.java.scrapper.services.GitHubService;
-import ru.tinkoff.edu.java.scrapper.services.StackOverflowService;
 import ru.tinkoff.edu.java.scrapper.web.dto.AddLinkRequest;
 import ru.tinkoff.edu.java.scrapper.web.dto.ApiErrorResponse;
-import ru.tinkoff.edu.java.scrapper.web.dto.GitHubResponse;
 import ru.tinkoff.edu.java.scrapper.web.dto.LinkResponse;
 import ru.tinkoff.edu.java.scrapper.web.dto.ListLinksResponse;
 import ru.tinkoff.edu.java.scrapper.web.dto.RemoveLinkRequest;
-import ru.tinkoff.edu.java.scrapper.web.dto.StackOverflowResponse;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -33,13 +27,6 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 public class LinkController {
 
     private static final String TG_HEADER = "Tg-Chat-Id";
-    private GitHubService gitHubService;
-    private StackOverflowService stackOverflowService;
-
-    public LinkController(GitHubService gitHubService, StackOverflowService stackOverflowService) {
-        this.gitHubService = gitHubService;
-        this.stackOverflowService = stackOverflowService;
-    }
 
     @GetMapping(consumes = APPLICATION_JSON_VALUE)
     @Operation(summary = "Получить все отслеживаемые ссылки",
