ab7ed2300fd5b1cf0ab5603ff07b26d02d440b60
dasu <kirill12093@mail.ru>
2023-03-27 20:15:11 +0300

    fix(webclient) : add beans into configuration

--- scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/configuration/HttpInterfaceClientConfiguration.java
+++ scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/configuration/HttpInterfaceClientConfiguration.java
@@ -1,55 +1,32 @@
 package ru.tinkoff.edu.java.scrapper.configuration;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.reactive.function.client.support.WebClientAdapter;
-import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
-import ru.tinkoff.edu.java.scrapper.clients.GitHubClient;
-import ru.tinkoff.edu.java.scrapper.clients.StackOverflowClient;
+import ru.tinkoff.edu.java.scrapper.clients.GitHubClientImpl;
+import ru.tinkoff.edu.java.scrapper.clients.StackOverflowClientImpl;
 
 @Configuration
 public class HttpInterfaceClientConfiguration {
 
-    StackOverflowProperties stackProperties;
+    private final GitHubProperties gitHubProperties;
 
-    GitHubProperties githubProperties;
-    public HttpInterfaceClientConfiguration(StackOverflowProperties stackProperties, GitHubProperties gitHubProperties) {
-        this.stackProperties= stackProperties;
-        this.githubProperties = gitHubProperties;
-    }
-    @Bean("gitHub")
-    public WebClient gitHubWebClient() {
-        return WebClient.builder()
-            .baseUrl(githubProperties.getUrl())
-            .build();
-    }
+    private final StackOverflowProperties stackOverflowProperties;
 
-    @Bean("stackOverflow")
-    public WebClient stackOverflowWebClient() {
-        return WebClient.builder()
-            .baseUrl(stackProperties.getUrl())
-            .build();
+    public HttpInterfaceClientConfiguration(GitHubProperties gitHubProperties,
+                                            StackOverflowProperties stackOverflowProperties) {
+        this.gitHubProperties = gitHubProperties;
+        this.stackOverflowProperties = stackOverflowProperties;
     }
 
     @Bean
-    public GitHubClient gitHubClient(@Qualifier("gitHub") WebClient webClient) {
-        HttpServiceProxyFactory httpServiceProxyFactory = HttpServiceProxyFactory
-            .builder(WebClientAdapter.forClient(webClient))
-            .build();
-
-        return httpServiceProxyFactory.createClient(GitHubClient.class);
+    GitHubClientImpl gitHubClient() {
+        return new GitHubClientImpl(gitHubProperties.getUrl());
     }
 
     @Bean
-    public StackOverflowClient stackOverflowClient(@Qualifier("stackOverflow") WebClient webClient) {
-        HttpServiceProxyFactory httpServiceProxyFactory = HttpServiceProxyFactory
-            .builder(WebClientAdapter.forClient(webClient))
-            .build();
-
-        return httpServiceProxyFactory.createClient(StackOverflowClient.class);
+    StackOverflowClientImpl stackOverflowClient() {
+        return new StackOverflowClientImpl(stackOverflowProperties.getUrl());
     }
 
 }
