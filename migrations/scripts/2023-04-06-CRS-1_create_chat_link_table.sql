5a37594e8dcdf63cbdebf694ced745b9a46fdd85
dasu <kirill12093@mail.ru>
2023-04-08 18:40:33 +0300

    (task3): add primary key to chat_link table

--- migrations/scripts/2023-04-06-CRS-1_create_chat_link_table.sql
+++ migrations/scripts/2023-04-06-CRS-1_create_chat_link_table.sql
@@ -1,5 +1,6 @@
 create table if not exists chat_link
 (
-    chat_id bigint primary key references chat(id) on update cascade on delete cascade,
-    link_id bigint primary key references  link(id) on update cascade on delete cascade
+    id bigint primary key,
+    chat_id bigint references chat(id) on update cascade on delete cascade,
+    link_id bigint references  link(id) on update cascade on delete cascade
 );
