eb21d012924946fb8aaa59e2fb8844629faefce6
dasu <kirill12093@mail.ru>
2023-04-13 13:32:28 +0300

    (task4): fix tests

---
+++ scrapper/src/test/java/AvailabilityOfTablesTest.java
@@ -0,0 +1,30 @@
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class AvailabilityOfTablesTest extends IntegrationEnvironment {
+
+    @Test
+    public void implement_migrationsIntoDatabase() {
+        try(Connection connection = MY_POSTGRES_CONTAINER.createConnection("")) {
+            assertTrue(tableExists(connection, "chat"));
+        } catch (SQLException e) {
+            fail("Unable to work with db!");
+        }
+    }
+
+
+
+    private boolean tableExists(Connection connection, String tableName) throws SQLException {
+        DatabaseMetaData meta = connection.getMetaData();
+        ResultSet resultSet = meta.getTables(null, null, tableName, new String[] {"TABLE"});
+
+        return resultSet.next();
+    }
+}
--- scrapper/src/test/java/IntegrationEnvironment.java
+++ scrapper/src/test/java/IntegrationEnvironment.java
@@ -1,3 +1,5 @@
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import liquibase.Contexts;
 import liquibase.LabelExpression;
 import liquibase.Liquibase;
@@ -5,60 +7,55 @@ import liquibase.database.Database;
 import liquibase.database.DatabaseFactory;
 import liquibase.database.jvm.JdbcConnection;
 import liquibase.exception.LiquibaseException;
-import liquibase.resource.ClassLoaderResourceAccessor;
 import liquibase.resource.DirectoryResourceAccessor;
-import lombok.extern.slf4j.Slf4j;
-import org.junit.Test;
+import org.testcontainers.containers.JdbcDatabaseContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 import javax.sql.DataSource;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.nio.file.Path;
 import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
+public abstract class IntegrationEnvironment implements AutoCloseable {
+    static final PostgreSQLContainer<?> MY_POSTGRES_CONTAINER;
 
-
-@Slf4j
-public class IntegrationEnvironment extends AbstractContainerBaseTest {
-
-    private final Path PATH_TO_MIGRATIONS = new File(".").toPath()
+    private static final Path PATH_TO_MIGRATIONS = new File(".").toPath()
             .toAbsolutePath()
             .getParent()
             .getParent()
             .resolve("migrations");
 
-    @Test
-    public void implement_migrationsIntoDatabase() {
+    static {
+        MY_POSTGRES_CONTAINER = new PostgreSQLContainer<>(PostgreSQLTestImages.POSTGRES_TEST_IMAGE);
+        MY_POSTGRES_CONTAINER.start();
         DataSource dataSource = getDataSource(MY_POSTGRES_CONTAINER);
-        assertDoesNotThrow(() -> startMigrations(dataSource));
         try {
-            assertTrue(tableExists(dataSource.getConnection(), "chat"));
-            assertTrue(tableExists(dataSource.getConnection(), "link"));
-            assertTrue(tableExists(dataSource.getConnection(), "chat_link"));
-
-        } catch (SQLException ex) {
-            System.out.println(ex.getMessage());
-            fail("Connection error!");
+            startMigrations(dataSource);
+        } catch (SQLException | LiquibaseException | FileNotFoundException e) {
+            throw new RuntimeException(e);
         }
     }
 
-    public void startMigrations(DataSource dataSource) throws SQLException, LiquibaseException, FileNotFoundException {
+    private static DataSource getDataSource(JdbcDatabaseContainer<?> container) {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(container.getJdbcUrl());
+        config.setUsername(container.getUsername());
+        config.setPassword(container.getPassword());
+        config.setDriverClassName(container.getDriverClassName());
+        return new HikariDataSource(config);
+    }
+
+    private static void startMigrations(DataSource dataSource) throws SQLException, LiquibaseException, FileNotFoundException {
         Connection connection = dataSource.getConnection();
         Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(connection));
         Liquibase liquibase = new Liquibase("master.xml", new DirectoryResourceAccessor(PATH_TO_MIGRATIONS), database);
-        log.info(liquibase.getChangeLogFile());
         liquibase.update(new Contexts(), new LabelExpression());
     }
 
-    private boolean tableExists(Connection connection, String tableName) throws SQLException {
-        DatabaseMetaData meta = connection.getMetaData();
-        ResultSet resultSet = meta.getTables(null, null, tableName, new String[] {"TABLE"});
-
-        return resultSet.next();
+    @Override
+    public void close() {
+        MY_POSTGRES_CONTAINER.close();
     }
 }
