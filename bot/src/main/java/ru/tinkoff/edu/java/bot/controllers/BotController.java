dc23c43c25595848d4d40539a187ac76f71f9e04
dasu <kirill12093@mail.ru>
2023-04-19 01:23:45 +0300

    (task 4) add bot reflection on update

--- bot/src/main/java/ru/tinkoff/edu/java/bot/controllers/BotController.java
+++ bot/src/main/java/ru/tinkoff/edu/java/bot/controllers/BotController.java
@@ -1,6 +1,7 @@
 package ru.tinkoff.edu.java.bot.controllers;
 
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,16 +10,25 @@ import org.springframework.web.bind.annotation.RestController;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import ru.tinkoff.edu.java.bot.dto.LinkUpdateRequest;
+import ru.tinkoff.edu.java.bot.utils.MessageSender;
+
+import java.util.Map;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 
 @RestController
+@RequiredArgsConstructor
 public class BotController {
+
+    private final MessageSender sender;
 
     @PostMapping(path = "/updates", produces = APPLICATION_JSON_VALUE)
     @Operation(summary = "Отправить обновление")
     public ResponseEntity<String> updateLink(@Valid @RequestBody LinkUpdateRequest request) {
-        return ResponseEntity.ok().body("OK");
+        for (long id : request.tgChatIds()) {
+            sender.send(id, "responses/update_link.mustache", Map.of("link", request.url()));
+        }
+        return ResponseEntity.ok("Updates received!");
     }
 }
