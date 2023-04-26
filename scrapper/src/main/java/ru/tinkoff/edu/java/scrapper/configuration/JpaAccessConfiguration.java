b8905d3b3d5316d8bf091cecbd9ba8243398eff4
dasu <kirill12093@mail.ru>
2023-04-26 10:31:28 +0300

    (task2): add jpa conditional configuration

---
+++ scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/configuration/JpaAccessConfiguration.java
@@ -0,0 +1,41 @@
+package ru.tinkoff.edu.java.scrapper.configuration;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.tinkoff.edu.java.scrapper.clients.BotClient;
+import ru.tinkoff.edu.java.scrapper.repository.jpa.JpaChatRepository;
+import ru.tinkoff.edu.java.scrapper.repository.jpa.JpaLinkRepository;
+import ru.tinkoff.edu.java.scrapper.scheduler.JpaLinkUpdater;
+import ru.tinkoff.edu.java.scrapper.scheduler.LinkUpdater;
+import ru.tinkoff.edu.java.scrapper.services.*;
+import ru.tinkoff.edu.java.scrapper.utils.LinkManager;
+
+@Configuration
+@ConditionalOnProperty(prefix = "app", name = "database-access-type", havingValue = "jpa")
+public class JpaAccessConfiguration {
+    @Bean
+    public LinkService linkService(
+            JpaLinkRepository linkRepository,
+            JpaChatRepository chatRepository,
+            LinkManager manager
+    ) {
+        return new JpaLinkService(linkRepository, chatRepository, manager);
+    }
+
+    @Bean
+    public TgChatService tgChatService(
+            JpaChatRepository chatRepository
+    ) {
+        return new JpaTgChatService(chatRepository);
+    }
+
+    @Bean
+    public LinkUpdater linkUpdater(
+            JpaLinkRepository linkRepository,
+            LinkManager manager,
+            BotClient botClient
+    ) {
+        return new JpaLinkUpdater(linkRepository,manager, botClient);
+    }
+}
